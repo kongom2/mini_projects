a99d719e6893bc9050a25ebeca0465c2d0c68c25
kongom2 <kongom2@jsceno.com>
2021-12-06 23:29:26 +0900

    버튼 추가

--- go99/src/elements/Button.js
+++ go99/src/elements/Button.js
@@ -1,7 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 
-const Button = () => {
-  return <React.Fragment>버튼 컴포넌트</React.Fragment>;
+const Button = (props) => {
+  const { text, _onClick, children, margin, width, padding } = props;
+  const styles = {
+    margin: margin,
+    width: width,
+    padding: padding,
+  };
+
+  return (
+    <React.Fragment>
+      <ElButton {...styles} onClick={_onClick}>
+        {text ? text : children}
+      </ElButton>
+    </React.Fragment>
+  );
 };
 
+Button.defaultProps = {
+  text: false,
+  children: null,
+  _onClick: () => {},
+  is_float: false,
+  margin: false,
+  width: "100%",
+  padding: "12px 0px",
+};
+
+const ElButton = styled.button`
+  width: ${(props) => props.width};
+  background-color: #d85147;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 18px;
+  padding: ${(props) => props.padding};
+  box-sizing: border-box;
+  border-radius: 6px;
+  border: none;
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+`;
 export default Button;
--- go99/src/elements/elementsIndex.js
+++ go99/src/elements/elementsIndex.js
@@ -2,5 +2,6 @@ import Input from "./Input";
 import Grid from "./Grid";
 import Text from "./Text";
 import AddButton from "./AddButton";
+import Button from "./Button";
 
-export {Input,Grid,Text,AddButton}
+export { Input, Grid, Text, AddButton, Button };
